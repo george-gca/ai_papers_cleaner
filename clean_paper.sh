94b54210d613c33acc081954182c9c79b363c01e
George Araujo <george.gcac@gmail.com>
2022-10-28 13:36:29 -0300

    Added clean_paper.sh
    Signed-off-by: George Araujo <george.gcac@gmail.com>

---
+++ clean_paper.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Enable poetry if not running inside docker and poetry is installed
+if [[ $HOSTNAME != "docker-"* ]] && (hash poetry 2>/dev/null); then
+    run_command="poetry run python text_cleaner.py -l debug"
+else
+    run_command="python text_cleaner.py -l debug"
+fi
+
+paper_separator='<#sep#>'
+# clean_abstracts=1
+clean_papers=1
+
+# index=565
+title="Moon IME: Neural-based Chinese Pinyin Aided Input Method with Customizable Association"
+conf=acl
+year=2018
+
+
+
+# clean papers abstracts
+if [ -n "$clean_abstracts" ]; then
+    if [ -n "$index" ]; then
+        $run_command -f data/$conf/$year/abstracts.csv -a -i $index
+    else
+        $run_command -f data/$conf/$year/abstracts.csv -a -t "$title"
+    fi
+fi
+
+# clean papers content
+if [ -n "$clean_papers" ]; then
+    if [ -n "$index" ]; then
+        $run_command -f data/$conf/$year/pdfs.csv -s "$paper_separator" -i $index
+    else
+        $run_command -f data/$conf/$year/pdfs.csv -s "$paper_separator" -t "$title"
+    fi
+fi
