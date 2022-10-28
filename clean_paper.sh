56b0e4f02f4b787c90980b85ad75c3440f32f50d
George Araujo <george.gcac@gmail.com>
2022-10-28 17:41:00 -0300

    Added header explanation to clean_paper.sh
    Signed-off-by: George Araujo <george.gcac@gmail.com>

--- clean_paper.sh
+++ clean_paper.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Use this script to check how the cleaning is working for a specific paper
+# it is useful to check the cleaning process step by step and to improve it
+
 # Enable poetry if not running inside docker and poetry is installed
 if [[ $HOSTNAME != "docker-"* ]] && (hash poetry 2>/dev/null); then
     run_command="poetry run python text_cleaner.py -l debug"
@@ -14,7 +17,6 @@ index=1
 # title="Moon IME: Neural-based Chinese Pinyin Aided Input Method with Customizable Association"
 conf=aaai
 year=2017
-
 
 
 # clean papers abstracts
